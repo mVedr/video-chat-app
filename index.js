2c58aecd1e82bbd45dbbb6c135642583bf85862a
ved2805 <vedrecharla@gmail.com>
2022-11-20 12:10:25 +0530

    second try

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ io.on("connection", (socket) => {
 	});
 });
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port `));
